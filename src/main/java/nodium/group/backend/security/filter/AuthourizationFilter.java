e37ef4cebeef82743919b085ebb1caf86205840b
Biokes <106444765+Biokes@users.noreply.github.com>
2024-08-30 19:50:02 +0100

    start authorization

--- src/main/java/nodium/group/backend/security/filter/AuthourizationFilter.java
+++ src/main/java/nodium/group/backend/security/filter/AuthourizationFilter.java
@@ -13,5 +13,6 @@ public class AuthourizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        filterChain.doFilter(request,response);
     }
 }
