008948638aef9ef51dd9e2d9a5f95ec0ad0e8d38
Biokes <106444765+Biokes@users.noreply.github.com>
2024-08-30 14:24:58 +0100

    update secured user model

--- src/main/java/nodium/group/backend/security/filter/AuthenticationFilter.java
+++ src/main/java/nodium/group/backend/security/filter/AuthenticationFilter.java
@@ -1,8 +1,6 @@
 package nodium.group.backend.security.filter;
 
-import lombok.AllArgsConstructor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
---
+++ src/main/java/nodium/group/backend/security/models/SecuredUser.java
@@ -0,0 +1,26 @@
+package nodium.group.backend.security.models;
+
+import lombok.AllArgsConstructor;
+import nodium.group.backend.data.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+@AllArgsConstructor
+public class SecuredUser implements UserDetails {
+    private final User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRole().stream().map(role -> new SimpleGrantedAuthority(role.name())).toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
