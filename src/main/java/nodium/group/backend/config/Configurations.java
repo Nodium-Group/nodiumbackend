e5f7281250cbe7cbef2f95b4efcd4fa08c03d007
Biokes <106444765+Biokes@users.noreply.github.com>
2024-09-09 08:57:30 +0100

    update cron

--- src/main/java/nodium/group/backend/config/Configurations.java
+++ src/main/java/nodium/group/backend/config/Configurations.java
@@ -46,7 +46,7 @@ public class Configurations {
         List<Tokens> allInvalidTokes = tokenRepository.findAll().stream().filter(Tokens::isBlackListed).toList();
         tokenRepository.deleteAll(allInvalidTokes);
     }
-    @Scheduled(cron = "0 */2 * * * *")
+    @Scheduled(cron = "0 0 0/2 * * *")
     public void deleteInvalidOtps(){
         List<OTP> uselessOtps = otpRepository.findAll().stream().filter(otp ->
                 Duration.between(otp.getTimeGenerated(),
